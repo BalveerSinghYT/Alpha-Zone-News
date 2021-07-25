dca524768cbb4d5862492ecfe2c81a9934ed82bf
Balveer Singh <balveersinghyt@gmail.com>
2021-07-25 20:33:31 +0530

    SMTP

--- QuickNews/settings.py
+++ QuickNews/settings.py
@@ -152,11 +152,11 @@ LOGOUT_REDIRECT_URL = '/'
 
 if not DEBUG:
       EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-      EMAIL_HOST_USER = "balveersinghyt@gmail.com"
+      EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
       EMAIL_HOST = 'smtp.gmail.com'
       EMAIL_PORT = 587
       EMAIL_USE_TLS = True
-      EMAIL_HOST_PASSWORD = "vavrazudgbenluim"
+      EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 else:
     EMAIL_BACKEND = (
