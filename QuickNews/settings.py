b1fdbc808155b0619c6116f84e8f070b3ffef171
Balveer Singh <balveersinghyt@gmail.com>
2021-07-29 19:51:58 +0530

    staticfiles

--- QuickNews/settings.py
+++ QuickNews/settings.py
@@ -62,6 +62,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'QuickNews.urls'
